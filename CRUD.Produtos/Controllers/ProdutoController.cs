053ce787c3153264f89494b1f553698e2b3931cc
p.dias <p.dias@CMP-PDIAS>
2021-05-24 19:54:54 -0300

    Criado controller para CRUD de Produtos

---
+++ CRUD.Produtos/Controllers/ProdutoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Produtos.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
+}
